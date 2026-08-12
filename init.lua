8b2ff1a71612781689490ec120f28844cc16c150
mjabloniec <mateusz.jabloniec@verestro.com>
2026-08-12 13:30:20 +0200

    enable gruvbox-material

--- init.lua
+++ init.lua
@@ -383,18 +383,17 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
-    },
-  }
+  vim.pack.add { gh 'sainnhe/gruvbox-material' }
+  vim.o.background = 'dark'
+  vim.g.gruvbox_material_background = 'soft'
+  vim.g.gruvbox_material_foreground = 'material'
+  vim.g.gruvbox_material_enable_italic = false
+  vim.g.gruvbox_material_better_performance = 1
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'gruvbox-material'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
